43ac8e2542178a6e0c5720c5b0b1056075c05845
zaqijr7 <zaqijr7@gmail.com>
2021-11-04 20:44:17 +0700

    learn c++

--- main.cpp
+++ main.cpp
@@ -1,19 +1,45 @@
 #include <iostream>
 using namespace std;
-int main() {
-  int a;
-  int b;
-    cout << "Input a!" << endl;
-    cin >> a;
-    cout << "Input b!" << endl;
-    cin >> b;
-    cout << "a + b = " << a + b << endl;
-    if (a + b > 100) {
-      cout << "Nilai adalah ratusan" << endl;
-    } else if (a + b >= 10 && a + b <= 99) {
-      cout << "Nilai adalah puluhan" << endl;
-    } else if (a + b >= 0 && a + b <= 9) {
-      cout << "Nilai adalah satuan" << endl ;
-    }
-    return 0;
+
+
+void introduceMySelf() {
+  char nama[16] = "Muhammad Zaqi A";
+  int usia = 20;
+  char pekerjaan[14] = "Web Developer";
+  cout << "Nama : " << nama << endl;
+  cout << "Old years : " << usia << endl;
+  cout << "Occupation : " << pekerjaan << endl;
+  cout << "=====================================" << endl;
+  
 }
+
+int luasDanKelilingPersegiPanjang () {
+  int panjang, lebar;
+  cout << "Masukan lebar :" << endl;
+  cin >> lebar;
+  if (cin.fail()) {
+    cout << "Please Input Number" << endl;
+    return 0;
+  }
+  cout << "Masukan panjang :" << endl;
+  cin >> panjang;
+  if (cin.fail()) {
+    cout << "Please Input Number" << endl;
+    return 0;
+  }
+  const int luas = panjang * lebar;
+  const int keliling = 2 * (panjang + lebar);
+  cout << "Luasnya addalah : " << luas << endl;
+  cout << "Kelilingnya adalah : " << keliling << endl;
+  return 0;
+}
+
+
+
+int main() {
+  introduceMySelf();
+  luasDanKelilingPersegiPanjang();
+  return 0;
+}
+
+
